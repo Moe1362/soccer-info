11da51bbd6806f52498f6226fbb8ad46eac02487
Mohammad Abbasi <mxabbasi@gmail.com>
2024-04-10 12:58:19 -0700

    create ne js file

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+
+
+
+//const url = 'https://soccer-football-info.p.rapidapi.com/managers/view/?i=5fb962e4a89ab6bd&l=en_US';
+//const options = {
+	//method: 'GET',
+	//headers: {
+		//'X-RapidAPI-Key': 'aae0c5abcemsh6bf0e83148c0bd5p195e59jsnb058a75e4341',
+		//'X-RapidAPI-Host': 'soccer-football-info.p.rapidapi.com'
+	//}
+//};
+
+//fetch(url, options).then(function(response) {
+    //return response.json();
+   
+//}).then(function(data) {
+   // console.log("soccer api");
+   // console.log(data);
+    
+//})
